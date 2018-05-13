6736ef44e5ec3cece46685e579e31c8fe223cc33
Gaëtan Renaudeau <gaetan.renaudeau@ledger.fr>
2018-05-13 18:04:05 +0200

    add gl-react & gl-react-contrast-saturation-brightness

--- Quoi/screens/EditPostScreen.js
+++ Quoi/screens/EditPostScreen.js
@@ -7,6 +7,7 @@ import {
   StyleSheet,
   Text,
   View,
+  Slider,
 } from 'react-native';
 import { BorderlessButton } from 'react-native-gesture-handler';
 import { Ionicons } from 'react-native-vector-icons';
@@ -14,10 +15,42 @@ import { withUser } from 'react-native-authentication-helpers';
 import { ImageManipulator } from 'expo';
 import { compose, graphql } from 'react-apollo';
 import gql from 'graphql-tag';
+import { Surface } from 'gl-react-expo';
+import { ContrastSaturationBrightness } from 'gl-react-contrast-saturation-brightness';
 
 import Button from '../components/Button';
 import Constants from '../util/Constants';
 import uploadImageAsync from '../util/uploadImageAsync';
+
+class Knob extends React.Component {
+  state = { value: this.props.value };
+  onValueChange = value => {
+    this.setState({ value });
+    if (this.props.live) {
+      this.props.onChange(value);
+    }
+  };
+  onSlidingComplete = () => {
+    this.props.onChange(this.state.value);
+  };
+  render() {
+    const { value } = this.state;
+    const { label, max, min, step } = this.props;
+    return (
+      <View style={{ flexDirection: 'column' }}>
+        <Text style={{ fontWeight: 'bold' }}>{label}</Text>
+        <Slider
+          minimumValue={min}
+          maximumValue={max}
+          step={step}
+          onValueChange={this.onValueChange}
+          onSlidingComplete={this.onSlidingComplete}
+          value={value}
+        />
+      </View>
+    );
+  }
+}
 
 class EditPostScreen extends React.Component {
   static navigationOptions = ({ navigation }) => {
@@ -34,7 +67,8 @@ class EditPostScreen extends React.Component {
         <Button
           enabled={!isUploadInProgress}
           onPress={navigation.dismiss}
-          containerStyle={{ paddingHorizontal: 15 }}>
+          containerStyle={{ paddingHorizontal: 15 }}
+        >
           <Ionicons name="ios-close" size={35} />
         </Button>
       ),
@@ -44,7 +78,8 @@ class EditPostScreen extends React.Component {
             !isUploadInProgress && !!navigation.getParam('onSubmit', false)
           }
           onPress={navigation.getParam('onSubmit')}
-          containerStyle={{ paddingHorizontal: 15 }}>
+          containerStyle={{ paddingHorizontal: 15 }}
+        >
           <Ionicons name="ios-checkmark" size={35} />
         </Button>
       ),
@@ -56,6 +91,9 @@ class EditPostScreen extends React.Component {
 
     this.state = {
       uri: props.navigation.getParam('originalUri'),
+      contrast: 1,
+      saturation: 1,
+      brightness: 1,
     };
   }
 
@@ -63,16 +101,58 @@ class EditPostScreen extends React.Component {
     this.props.navigation.setParams({ onSubmit: this._createPostAsync });
   }
 
+  onSurfaceRef = surface => {
+    this.surface = surface;
+  };
+
   render() {
+    const { contrast, saturation, brightness } = this.state;
     return (
       <View style={{ flex: 1 }}>
-        <Image
-          source={{ uri: this.state.uri }}
+        <Surface
+          ref={this.onSurfaceRef}
           style={{
             width: Constants.screen.width,
             height: Constants.screen.width,
           }}
-        />
+        >
+          <ContrastSaturationBrightness
+            contrast={contrast}
+            saturation={saturation}
+            brightness={brightness}
+          >
+            {{ uri: this.state.uri }}
+          </ContrastSaturationBrightness>
+        </Surface>
+        <View>
+          <Knob
+            label="contrast"
+            onChange={contrast => this.setState({ contrast })}
+            value={contrast}
+            min={0}
+            max={3}
+            step={0.1}
+            live
+          />
+          <Knob
+            label="saturation"
+            onChange={saturation => this.setState({ saturation })}
+            value={contrast}
+            min={0}
+            max={3}
+            step={0.1}
+            live
+          />
+          <Knob
+            label="brightness"
+            onChange={brightness => this.setState({ brightness })}
+            value={brightness}
+            min={0}
+            max={3}
+            step={0.1}
+            live
+          />
+        </View>
         {this._maybeRenderLoading()}
       </View>
     );
@@ -81,7 +161,7 @@ class EditPostScreen extends React.Component {
   _maybeRenderLoading = () => {
     let uploadProgress = this.props.navigation.getParam(
       'uploadProgress',
-      false
+      false,
     );
     let isUploadInProgress = !!uploadProgress;
 
@@ -98,7 +178,8 @@ class EditPostScreen extends React.Component {
             alignItems: 'center',
             paddingTop: Constants.screen.width / 2 - 25,
           },
-        ]}>
+        ]}
+      >
         <ActivityIndicator size="large" color="#fff" />
         <Text style={{ fontSize: 16, marginTop: 10, color: '#fff' }}>
           {uploadProgress}
@@ -110,8 +191,12 @@ class EditPostScreen extends React.Component {
   _createPostAsync = async () => {
     try {
       this.props.navigation.setParams({ uploadProgress: 'Preparing image' });
-      let { uri } = this.state;
+
       let { ticketId, email, firstName } = this.props.user;
+      let { uri } = await this.surface.glView.capture({
+        format: 'png',
+      });
+
       let {
         uri: resizedUri,
         width,
@@ -119,11 +204,12 @@ class EditPostScreen extends React.Component {
       } = await ImageManipulator.manipulate(
         uri,
         [{ resize: { width: 500, height: 500 } }],
-        { format: 'png' }
+        { format: 'png' },
       );
+
       this.props.navigation.setParams({ uploadProgress: 'Uploading image' });
       let fileUrl = await uploadImageAsync(resizedUri, ticketId);
-      console.log({fileUrl})
+      console.log({ fileUrl });
       this.props.navigation.setParams({ uploadProgress: 'Creating post' });
       await this.props.createAuthenticatedPost({
         variables: {
@@ -147,7 +233,7 @@ class EditPostScreen extends React.Component {
           { text: 'Cancel', onPress: () => {}, style: 'cancel' },
           { text: 'Retry', onPress: this._createPostAsync },
         ],
-        { cancelable: false }
+        { cancelable: false },
       );
     }
   };
@@ -192,5 +278,5 @@ const ADD_POST_MUTATION = gql`
 
 export default compose(
   graphql(ADD_POST_MUTATION, { name: 'createAuthenticatedPost' }),
-  withUser
+  withUser,
 )(EditPostScreen);
